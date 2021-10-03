4b0f7b066c8fe4a077eed35ccdbd32a24f3b16f4
Jaden Giordano <thejadenjack@gmail.com>
2021-10-02 22:16:54 -0700

    add simple input demonstration

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="w-screen h-screen relative">
     <widget title="A cool widget">
-      <span>content</span>
+      <span>current value: {{wave}}</span>
     </widget>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { observableRef, bind } from './context';
 import Widget from './components/Widget.vue';
+import { interval, map } from 'rxjs';
 
 export default defineComponent({
   components: {
     Widget,
+  },
+  setup() {
+    const wave$ = bind(interval(500).pipe(
+      map((index) => index % 2 == 1 ? -1 : 1),
+    ));
+    const wave = observableRef<number>(wave$);
+
+    return { wave };
   },
 });
 </script>
