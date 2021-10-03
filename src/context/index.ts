4a39b7f979c089b40dc79d84dbc8cb11e4ce8e67
Jaden Giordano <thejadenjack@gmail.com>
2021-10-02 22:09:11 -0700

    rxjs util

---
+++ src/context/index.ts
@@ -0,0 +1,19 @@
+import { DeepReadonly, onBeforeUnmount, readonly, ref } from "vue";
+import { Observable, share } from 'rxjs';
+
+type ReadonlyRef<T = unknown> = {
+  readonly value?: DeepReadonly<T>;
+};
+
+export function observableRef<T = unknown>(source$: Observable<T>): ReadonlyRef {
+  const reference = ref<T>();
+  const sub = source$.subscribe((value) => {
+    reference.value = value;
+  });
+  onBeforeUnmount(() => sub.unsubscribe());
+  return readonly(reference);
+}
+
+export function bind<T = unknown>(source$: Observable<T>): Observable<T> {
+  return source$.pipe(share());
+}
